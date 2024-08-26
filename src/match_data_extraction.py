ed8e762744aa005d1004b56c593ebc3941546105
TomasGlau <tomigesita@gmail.com>
2024-08-26 17:41:51 -0300

    feat(get_jugadores)

--- src/match_data_extraction.py
+++ src/match_data_extraction.py
@@ -14,7 +14,7 @@ def get_jugadores(lineup: EventosLineup) -> List[Jugador]:
         List[Jugador]: Jugadores del lineup
     """
 
-    return [lineup["playerName"].unique()]
+    return [lineup["player_id"].unique()]
 
 
 def get_passes(lineup: EventosLineup, jugador: Jugador, jugador2: Jugador) -> int:

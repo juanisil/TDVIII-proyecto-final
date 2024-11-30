e6c58c47db1f6323f026ab4b16fcee0cd53fb1ec
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2024-11-30 09:38:22 -0300

    fix(distill)

---
+++ recursos_pdf/mathjax-config.js
@@ -0,0 +1,8 @@
+MathJax = {
+    tex: {
+        packages: { '[+]': ['color'] }, // Enable the color package
+    },
+    options: {
+        enableMenu: false // Optional: Disable the MathJax menu for simplicity
+    }
+};
